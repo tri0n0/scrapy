ec78825f760f85c6bb42025fa1caa8341cc9a48d
tri0n0 <johnhugh723@gmail.com>
2024-10-10 20:24:33 +0530

    Drop Python 3.8 support and update tox.ini for Python 3.9+

--- setup.py
+++ setup.py
@@ -57,8 +57,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -69,7 +67,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=install_requires,
     extras_require=extras_require,
 )
